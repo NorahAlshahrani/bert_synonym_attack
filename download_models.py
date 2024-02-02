4c3d0dd489e187fc44dc4981202827b7fcf46aa9
Saied Alshahrani <101026149+SaiedAlshahrani@users.noreply.github.com>
2024-02-01 19:09:45 -0500

    Push Python Scripts

---
+++ download_models.py
@@ -0,0 +1,81 @@
+import os; from huggingface_hub import login
+
+cmd = "git clone https://huggingface.co/aubmindlab/bert-base-arabertv2"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {bert-base-arabertv2} is successful!")
+else: print("# Downloading {bert-base-arabertv2} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/BERTmsda"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {BERTmsda} is successful!")
+else: print("# Downloading {BERTmsda} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/BERThard"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {BERThard} is successful!")
+else: print("# Downloading {BERThard} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/BERT_hard_adversarial"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {BERT_hard_adversarial} is successful!")
+else: print("# Downloading {BERT_hard_adversarial} is failed!")
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/BERT_msda_adversarial"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {BERT_msda_adversarial} is successful!")
+else: print("# Downloading {BERT_msda_adversarial} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/Adv_BERT_Hard"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {Adv_BERT_Hard} is successful!")
+else: print("# Downloading {Adv_BERT_Hard} is failed!")
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/Adv_BERT_msda"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {Adv_BERT_msda} is successful!")
+else: print("# Downloading {Adv_BERT_msda} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/2dCNNhard"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {2dCNNhard} is successful!")
+else: print("# Downloading {2dCNNhard} is failed!")
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/2dCNNmsda"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {2dCNNmsda} is successful!")
+else: print("# Downloading {2dCNNmsda} is failed!")
+
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/biLSTMhard"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {biLSTMhard} is successful!")
+else: print("# Downloading {biLSTMhard} is failed!")
+
+cmd = "git clone https://huggingface.co/NorahAlshahrani/biLSTMmsda"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {biLSTMmsda} is successful!")
+else: print("# Downloading {biLSTMmsda} is failed!")
+
+
+cmd = "git clone https://huggingface.co/CAMeL-Lab/bert-base-arabic-camelbert-ca-pos-egy"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {bert-base-arabic-camelbert-ca-pos-egy} is successful!")
+else: print("# Downloading {bert-base-arabic-camelbert-ca-pos-egy} is failed!")
+
+
+cmd = "git clone https://huggingface.co/aubmindlab/bert-base-arabertv02"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {bert-base-arabertv02} is successful!")
+else: print("# Downloading {bert-base-arabertv02} is failed!")
+
+
+cmd = "git clone https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+exit_msg = os.system(cmd)
+if exit_msg==0: print("# Downloading {paraphrase-multilingual-mpnet-base-v2} is successful!")
+else: print("# Downloading {paraphrase-multilingual-mpnet-base-v2} is failed!")
+
